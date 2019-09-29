aaf05d5afc9fd865588ca9bf875bc3f45a461743
Gitongamiriam <miriamgitonga2019@gmail.com>
2019-09-29 21:23:03 +0300

    Add paragraph font-size

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#hide1,#show1").click(function() {
+        $("#show1").toggle();
+        $("#hide1").toggle();
+    });
+    $("#hide2,#show2").click(function() {
+        $("#show2").toggle();
+        $("#hide2").toggle();
+    });
+    $("#hide3,#show3").click(function() {
+        $("#show3").toggle();
+        $("#hide3").toggle();
+    });
+});
